050bf8b2c07fc88acf258fa656df8957ebf262de
M krishna chaitanya <krishnachaitanyajnv22@gmail.com>
2019-08-24 20:02:50 +0530

    Update face.py

--- face.py
+++ face.py
@@ -5,7 +5,7 @@ cap = cv2.VideoCapture(0)
 while 1:
 	ret,img = cap.read()
 	gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-	faces = face_cascad.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=5,minSize=(35,35))
+	faces = face_cascad.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5,minSize=(35,35))
 	for (x,y,w,h) in faces:
 		cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2)
 		roi_gray = gray[y:y+h, x:x+w]
